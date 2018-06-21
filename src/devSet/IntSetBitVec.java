1f35eb65efbd7a84fd8eb87e361a44e4f9d0f78f
KWONWON <38065924+KwonWon@users.noreply.github.com>
2018-06-21 15:36:52 +0900

    Update IntSetBitVec.java

--- src/devSet/IntSetBitVec.java
+++ src/devSet/IntSetBitVec.java
@@ -1,1 +1,46 @@
+import java.util.*;
 
+public class IntSetBitVec {
+	private Vector<Boolean> bitvec;
+	private int maxval, maxelems;
+	private int size;
+	
+	public IntSetBitVec(int maxval, int maxelems) {
+		bitvec = new Vector<Boolean>();
+		this.maxval = maxval;
+		this.maxelems = maxelems;
+		this.size = 0;
+		
+		for (int i = 0; i <= maxval; i++) {
+			bitvec.add(i, false);
+		}
+	}
+	
+	public void insert(int element) {
+		if ((size >= maxelems) | (element > maxval)) {
+			return;
+		}
+		if(bitvec.get(element)) {
+			return;
+		} 
+		else {
+			bitvec.remove(element);
+			bitvec.add(element, true);
+			size++;
+		}
+	}
+	
+	public int size() {
+		return this.size;
+	}
+	
+	public void report(int [] v) {
+		int j = 0;
+		for(int i = 0; i <= maxval; i++) {
+			if(bitvec.get(i)) {
+				v[j++] = i;
+			}
+		}
+	}
+	
+}
