b0981f3be52f6114966ff2d6a246618a5d9eeb98
KWONWON <38065924+KwonWon@users.noreply.github.com>
2018-06-21 17:02:23 +0900

    Update IntSetBitVec.java

--- src/devSet/IntSetBitVec.java
+++ src/devSet/IntSetBitVec.java
@@ -1,47 +1,60 @@
 package devSet;
 import java.util.*;
 
-public class IntSetBitVec {
-	private Vector<Boolean> bitvec;
-	private int maxval, maxelems;
+class IntSetBitVec implements IntSet {
+	
+	private byte[] bits;
 	private int size;
+	private int count = -1;
+	private int max_val;
+	private int max_length;
 	
-	public IntSetBitVec(int maxval, int maxelems) {
-		bitvec = new Vector<Boolean>();
-		this.maxval = maxval;
-		this.maxelems = maxelems;
-		this.size = 0;
-		
-		for (int i = 0; i <= maxval; i++) {
-			bitvec.add(i, false);
-		}
+	public IntSetBitVec() {
+
 	}
 	
+	@Override
+	public void intSetImp(int maxelems, int maxval) {
+		// construct bit vector capable of holding n bits.
+		bits = new byte[(maxval >> 3) + 1]; // 1byte = 8 bits
+		max_length = (maxval >> 3) + 1;
+		for(int i = 0; i < max_length; i++) {
+			bits[i] = 0;
+		}
+		size = 0;
+		max_val = maxval;
+	}
+
+	@Override
 	public void insert(int element) {
-		if ((size >= maxelems) | (element > maxval)) {
+		int abs_element = Math.abs(element);
+		if(size >= max_val) {
+			throw new ArrayIndexOutOfBoundsException("max_length exceeded.");
+		}
+		if((bits[abs_element >> 3] & (1<<(abs_element & 7))) != 0) {
 			return;
 		}
-		if(bitvec.get(element)) {
-			return;
-		} 
-		else {
-			bitvec.remove(element);
-			bitvec.add(element, true);
-			size++;
-		}
+		bits[abs_element>>3] |= 1 << (abs_element& 7);
+		size += 1;
 	}
-	
+
+	@Override
 	public int size() {
-		return this.size;
+		return size;
 	}
-	
-	public void report(int [] v) {
-		int j = 0;
-		for(int i = 0; i <= maxval; i++) {
-			if(bitvec.get(i)) {
-				v[j++] = i;
+
+	@Override
+	public int[] report() {
+		// TODO Auto-generated method stub
+		int[] result = new int[size];
+		int result_idx = 0;
+		for(int i = 0;i<=max_val;i++) {
+			if((bits[i >> 3] & (1<<(i & 7))) != 0) {
+				result[result_idx] = i;
+				result_idx++;
 			}
 		}
+		return result;
 	}
-	
+
 }
